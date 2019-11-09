36d62b48eec39de7e2e4f80d69c86ada46468792
Emily Wang <ezwang@berkeley.edu>
2019-11-08 19:36:49 -0800

    added Router and Main n Navigation files

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+
+export default function Main() {
+  return(
+    <Switch>
+      <Route path="">
+      </Route>
+      <Route path="">
+      </Route>
+    </Switch>
+  )
+}
