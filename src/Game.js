274ef62cfb2a7f2f2bb58ceb7d0ecebb9f25056a
Emily Wang <ezwang@berkeley.edu>
2019-12-02 19:55:15 -0800

    deleted extra line

--- src/Game.js
+++ src/Game.js
@@ -166,8 +166,6 @@ export default class Game extends React.Component {
               </div>
             </div>
           </div>
-          <canvas id="myChart" width="400" height="400"></canvas>
-
         </div>
         <br/>
         <div>
