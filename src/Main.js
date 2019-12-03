9df312c662da54472e01419f31fb6650ddb0d8f1
Angela Xu <angelazxu@berkeley.edu>
2019-12-02 19:54:22 -0800

    helo

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
+import Navigation from './Navigation.js';
 import News from './News.js';
 export default function Main() {
   return(
     <Switch>
-      <Route path="">
+
+      <Route path="/News">
           <News/>
       </Route>
     </Switch>
--- src/News.js
+++ src/News.js
@@ -46,7 +46,19 @@ class News extends React.Component {
     }
     render(){
         return (
+            <div>
+            <h1>News</h1>
+            <div align="center" padding="20%" margin="30%">
+                <div class="ui search">
+                <div class="ui icon input">
+                  <input class="prompt" type="text" placeholder="Search news..."/>
+                  <i class="search icon"></i>
+                </div>
+                <div class="results"></div>
+              </div>
+            </div>
             <div>{this.APIcall()}</div>
+            </div>
         );
     }
 }
