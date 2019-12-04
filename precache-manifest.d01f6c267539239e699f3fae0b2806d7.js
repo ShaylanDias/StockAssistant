5a44a83940d2760b2d5cfc1e70b5b381331c4320
Shaylan Dias <shaylandias@gmail.com>
2019-12-03 21:35:54 -0800

    Updates

---
+++ precache-manifest.d01f6c267539239e699f3fae0b2806d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc37f6792bb2e2f4317ffc1545751d49",
+    "url": "/StockAssistant/index.html"
+  },
+  {
+    "revision": "d57801f6e0e4160fa49a",
+    "url": "/StockAssistant/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7a6116ff5907f6a4cb4c",
+    "url": "/StockAssistant/static/js/2.df8e1dcb.chunk.js"
+  },
+  {
+    "revision": "d57801f6e0e4160fa49a",
+    "url": "/StockAssistant/static/js/main.4b3755cf.chunk.js"
+  },
+  {
+    "revision": "d0bdf532891ba9288c27",
+    "url": "/StockAssistant/static/js/runtime-main.b5027f70.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/StockAssistant/static/media/logo.25bf045c.svg"
+  }
+]);
